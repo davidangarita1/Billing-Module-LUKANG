85eb334625869727b897681767b95cac79d35177
David H. Angarita <davidangarita.sistemas@gmail.com>
2021-11-30 19:57:35 -0500

    Se crea la pagina de no encontrado

---
+++ front/src/components/NotFound.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+const NotFound = () => {
+	return (
+		<Fragment>
+			<p>La página no fue encontrada, porfavor escriba una URL valida.</p>
+		</Fragment>
+	);
+}
+ 
+export default NotFound;
