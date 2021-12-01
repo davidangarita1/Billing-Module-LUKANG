ff887afcbd5c0e3da0ecc7240d3fe3eb8dadc5d7
David H. Angarita <davidangarita.sistemas@gmail.com>
2021-11-30 19:55:46 -0500

    Se eliminan recursos innecesarios

--- front/src/api/ProductService.js
+++
@@ -1,8 +0,0 @@
-import HttpClient from './API';
-
-const getAll = () => {
-	return HttpClient.get('/products')
-}
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {getAll};
