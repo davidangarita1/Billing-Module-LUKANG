fa806677a8eaa304622282fffd57c3bb35c4a72f
David H. Angarita <davidangarita.sistemas@gmail.com>
2021-12-01 23:04:13 -0500

    Se añade el home y nuevas rutas

--- front/src/App.js
+++ front/src/App.js
@@ -6,19 +6,32 @@ import Navbar from './common/Navbar';
 import ProductList from './components/Product/ProductList';
 import AddProduct from './components/Product/AddProduct';
 import ClientList from './components/Client/ClientList';
+import AddClient from './components/Client/AddClient';
+import CategoryList from './components/Category/CategoryList';
+import AddCategory from './components/Category/AddCategory';
+import InvoiceList from './components/Invoice/InvoiceList';
+import AddInvoice from './components/Invoice/AddInvoice';
 import NotFound from './common/NotFound';
-
+import Home from './common/Home';
 
 const App = () => {
   return (
       <Router>
         <Navbar />
           <Switch>
-            <Route exact path="/" component={ProductList} />
+            <Route exact path="/" component={Home} />
             <Route path="/products" component={ProductList} />
             <Route path="/add-product" component={AddProduct} />
             <Route path="/product/edit/:id" component={AddProduct} />
             <Route path="/clients" component={ClientList} />
+            <Route path="/add-client" component={AddClient} />
+            <Route path="/client/edit/:id" component={AddClient} />
+            <Route path="/categories" component={CategoryList} />
+            <Route path="/add-category" component={AddCategory} />
+            <Route path="/category/edit/:id" component={AddCategory} />
+            <Route path="/invoices" component={InvoiceList} />
+            <Route path="/add-invoice" component={AddInvoice} />
+            <Route path="/invoice/edit/:id" component={AddInvoice} />
             <Route path="*" component={NotFound} />
           </Switch>
       </Router>
---
+++ front/src/common/Home.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+
+const Home = () => {
+	return (
+		<Fragment>
+			<div className="container">
+				<div className="row text-center mt-3">
+					<div className="col-md-12">
+						<h1>Panaderia LUKANG</h1>
+						<img src="https://cdn.pixabay.com/photo/2017/05/27/23/12/bread-2349711_960_720.jpg" className="img-fluid" alt="Panaderia LUKANG" />
+					</div>
+				</div>
+
+			</div>
+		</Fragment>
+	);
+}
+
+export default Home;
